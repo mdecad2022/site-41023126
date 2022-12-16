910a2727c05f0a1a63853a253d75727eb69d52ec
41023126 <41023126@gm.nfu.edu.tw>
2022-12-16 13:42:27 +0000

    hw14 nx 影片新增

---
+++ downloads/hw14_nx.js
@@ -0,0 +1,11 @@
+﻿var winkVideoData = {
+  dataVersion: 1,
+  frameRate: 10,
+  buttonFrameLength: 5,
+  buttonFrameOffset: 2,
+  frameStops: {
+    "1143": [
+      { type: "gotoframe", rect: { x: 1142, y: 205, width: 60, height: 24 }, target: 0 },
+    ],
+  },
+};
