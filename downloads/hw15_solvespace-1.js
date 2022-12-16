4fc70519a02e10a41bb456fd078e0b731b2bdce2
41023126 <41023126@gm.nfu.edu.tw>
2022-12-16 07:50:48 +0000

    hw15 hw15 solvespace 繪圖過程

---
+++ downloads/hw15_solvespace-1.js
@@ -0,0 +1,23 @@
+﻿var winkVideoData = {
+  dataVersion: 1,
+  frameRate: 10,
+  buttonFrameLength: 5,
+  buttonFrameOffset: 2,
+  frameStops: {
+    "651": [
+      { type: "gotoframe", rect: { x: 635, y: 513, width: 60, height: 24 }, target: 0 },
+      { type: "gotoframe", rect: { x: 1188, y: 674, width: 60, height: 24 }, target: 652 },
+    ],
+    "750": [
+      { type: "gotoframe", rect: { x: 577, y: 410, width: 60, height: 24 }, target: 651 },
+      { type: "gotoframe", rect: { x: 874, y: 613, width: 60, height: 24 }, target: 751 },
+    ],
+    "830": [
+      { type: "gotoframe", rect: { x: 863, y: 378, width: 60, height: 24 }, target: 750 },
+      { type: "gotoframe", rect: { x: 1146, y: 632, width: 60, height: 24 }, target: 831 },
+    ],
+    "849": [
+      { type: "gotoframe", rect: { x: 867, y: 211, width: 60, height: 24 }, target: 830 },
+    ],
+  },
+};
