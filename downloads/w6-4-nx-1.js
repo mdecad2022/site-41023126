6670ca23e4e8afb8a5ee17946537e93c605be0a4
41023126 <41023126@gm.nfu.edu.tw>
2022-12-31 07:53:02 +0000

    補交w6-零件五

---
+++ downloads/w6-4-nx-1.js
@@ -0,0 +1,14 @@
+﻿var winkVideoData = {
+  dataVersion: 1,
+  frameRate: 10,
+  buttonFrameLength: 5,
+  buttonFrameOffset: 2,
+  frameStops: {
+    "1151": [
+      { type: "gotoframe", rect: { x: 538, y: 262, width: 60, height: 24 }, target: 0 },
+    ],
+    "1255": [
+      { type: "gotoframe", rect: { x: 951, y: 662, width: 60, height: 24 }, target: 1256 },
+    ],
+  },
+};
