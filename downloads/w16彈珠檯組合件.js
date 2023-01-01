966d5e636655b15d1070ce37c8bdf436e990f86e
41023126 <41023126@gm.nfu.edu.tw>
2023-01-01 06:38:25 +0000

    新增w16-彈珠檯 繪製及組合過程

---
+++ downloads/w16彈珠檯組合件.js
@@ -0,0 +1,12 @@
+﻿var winkVideoData = {
+  dataVersion: 1,
+  frameRate: 10,
+  buttonFrameLength: 5,
+  buttonFrameOffset: 2,
+  frameStops: {
+    "721": [
+      { type: "gotoframe", rect: { x: 831, y: 85, width: 60, height: 24 }, target: 0 },
+      { type: "gotoframe", rect: { x: 454, y: 62, width: 60, height: 24 }, target: 722 },
+    ],
+  },
+};
