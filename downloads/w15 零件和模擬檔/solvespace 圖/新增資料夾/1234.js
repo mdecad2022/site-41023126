251615f845367ed37f2be56a868b34491357bec3
41023126 <41023126@gm.nfu.edu.tw>
2023-01-06 02:15:18 +0000

    補放 w14w15的零件檔和模擬檔的連結

---
+++ downloads/w15 零件和模擬檔/solvespace 圖/新增資料夾/1234.js
@@ -0,0 +1,33 @@
+﻿var winkVideoData = {
+  dataVersion: 1,
+  frameRate: 10,
+  buttonFrameLength: 5,
+  buttonFrameOffset: 2,
+  frameStops: {
+    "173": [
+      { type: "gotoframe", rect: { x: 36, y: 541, width: 60, height: 24 }, target: 0 },
+    ],
+    "258": [
+      { type: "gotoframe", rect: { x: 989, y: 590, width: 60, height: 24 }, target: 173 },
+    ],
+    "263": [
+      { type: "gotoframe", rect: { x: 1361, y: 249, width: 60, height: 24 }, target: 258 },
+    ],
+    "268": [
+      { type: "gotoframe", rect: { x: 516, y: 416, width: 60, height: 24 }, target: 263 },
+      { type: "gotoframe", rect: { x: 1309, y: 329, width: 60, height: 24 }, target: 269 },
+    ],
+    "357": [
+      { type: "gotoframe", rect: { x: 1172, y: 691, width: 60, height: 24 }, target: 358 },
+      { type: "gotoframe", rect: { x: 606, y: 394, width: 60, height: 24 }, target: 268 },
+    ],
+    "568": [
+      { type: "gotoframe", rect: { x: 980, y: 289, width: 60, height: 24 }, target: 569 },
+      { type: "gotoframe", rect: { x: 1321, y: 380, width: 60, height: 24 }, target: 357 },
+    ],
+    "575": [
+      { type: "gotoframe", rect: { x: 1045, y: 203, width: 60, height: 24 }, target: 568 },
+      { type: "gotoframe", rect: { x: 1243, y: 344, width: 60, height: 24 }, target: 576 },
+    ],
+  },
+};
