dc7f3092375078a9fa2404d2c7a02ffc984ae688
41023126 <41023126@gm.nfu.edu.tw>
2023-01-06 02:40:24 +0000

    更改部分內容

---
+++ downloads/w6/4-nx-2.js
@@ -0,0 +1,14 @@
+﻿var winkVideoData = {
+  dataVersion: 1,
+  frameRate: 10,
+  buttonFrameLength: 5,
+  buttonFrameOffset: 2,
+  frameStops: {
+    "608": [
+      { type: "gotoframe", rect: { x: 1336, y: 521, width: 60, height: 24 }, target: 0 },
+    ],
+    "942": [
+      { type: "gotoframe", rect: { x: 1227, y: 346, width: 60, height: 24 }, target: 608 },
+    ],
+  },
+};
---
+++ downloads/w6/5-nx.js
@@ -0,0 +1,23 @@
+﻿var winkVideoData = {
+  dataVersion: 1,
+  frameRate: 10,
+  buttonFrameLength: 5,
+  buttonFrameOffset: 2,
+  frameStops: {
+    "502": [
+      { type: "gotoframe", rect: { x: 1511, y: 679, width: 60, height: 24 }, target: 0 },
+    ],
+    "768": [
+      { type: "gotoframe", rect: { x: 1394, y: 383, width: 60, height: 24 }, target: 502 },
+    ],
+    "773": [
+      { type: "gotoframe", rect: { x: 1538, y: 705, width: 60, height: 24 }, target: 768 },
+    ],
+    "1137": [
+      { type: "gotoframe", rect: { x: 1476, y: 547, width: 60, height: 24 }, target: 773 },
+    ],
+    "1142": [
+      { type: "gotoframe", rect: { x: 749, y: 562, width: 60, height: 24 }, target: 1143 },
+    ],
+  },
+};
