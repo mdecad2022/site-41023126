c53800e87fdce7801311a09d69c1f3ee822a66a5
41023126 <41023126@gm.nfu.edu.tw>
2023-01-07 08:10:34 +0000

    w6 鑽床13個零件

---
+++ downloads/w6drill_press零件檔/Feed handle axle/Feed handle axle.js
@@ -0,0 +1,11 @@
+﻿var winkVideoData = {
+  dataVersion: 1,
+  frameRate: 10,
+  buttonFrameLength: 5,
+  buttonFrameOffset: 2,
+  frameStops: {
+    "373": [
+      { type: "gotoframe", rect: { x: 697, y: 668, width: 60, height: 24 }, target: 374 },
+    ],
+  },
+};
---
+++ downloads/w6drill_press零件檔/base/base.js
@@ -0,0 +1,17 @@
+﻿var winkVideoData = {
+  dataVersion: 1,
+  frameRate: 10,
+  buttonFrameLength: 5,
+  buttonFrameOffset: 2,
+  frameStops: {
+    "380": [
+      { type: "gotoframe", rect: { x: 884, y: 771, width: 60, height: 24 }, target: 381 },
+    ],
+    "553": [
+      { type: "gotoframe", rect: { x: 1170, y: 753, width: 60, height: 24 }, target: 380 },
+    ],
+    "865": [
+      { type: "gotoframe", rect: { x: 979, y: 512, width: 60, height: 24 }, target: 553 },
+    ],
+  },
+};
